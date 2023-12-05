1757aaa3bbafccdb6edb2d49c3da916fc01ddfb3
ijasinsky <ijasinsky@nigul.cide.es>
2023-12-05 18:34:57 +0100

    Act21_3echo

---
+++ Lenguaje/ACT21/Actividad21_3.js
@@ -0,0 +1,13 @@
+const arrays = [1, 2, 3, 4, 5]
+
+let sumar = arrays[0] + arrays[1] + arrays[2] + arrays[3] + arrays[4]
+console.log (sumar)
+
+let media = sumar / 2
+console.log (media)
+
+let max = 
+console.log (Math.max(...arrays))
+
+let min = 
+console.log (Math.min(...arrays))
