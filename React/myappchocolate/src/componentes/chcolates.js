67783ca428ab39d0f45c671e852ed8c77eec26c4
ijasinsky <ijasinsky@nigul.cide.es>
2024-04-16 18:48:33 +0200

    quemas

--- React/myappchocolate/src/componentes/chcolates.js
+++ React/myappchocolate/src/componentes/chcolates.js
@@ -2,25 +2,27 @@ import React from "react";
 import './App.css';
 import { json, text } from "stream/consumers";
 
+function chocolates(){
+
 const requestURL = "C:\\Users\\alumne-DAM\\Documents\\Lenguaje-Marcas\\Lenguaje\\Activitat3.15\\data.json";
 
-function fetchQuestions(){
+function fetchQuestions() {
     fetch("C:\\Users\\alumne-DAM\\Documents\\Lenguaje-Marcas\\Lenguaje\\Activitat3.15\\data.json")
-    .then (response => response.json())
-    .then(result => console.log(result))
-    .catch(err => console.error(err))
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(err => console.error(err))
 }
 
 const fs = require('fs')
 
-fs.readFileSync('./data', function(err, result){
-    if(err) {
+fs.readFileSync('./data', function (err, result) {
+    if (err) {
         throw err;
     }
     console.log("El contenido es: ", JSON.parse(result))
 })
 
-const request = new data.json;
+const request = new fs.appendFile.json();
 request.open("GET", requestURL)
 
 request.responseType = "json";
@@ -29,41 +31,42 @@ request.send();
 const obj = JSON.parse(fetchQuestions)
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
-            items : {},
+            items: {},
             DataisLoaded: false
         };
     }
-    componentDidMount(){
+    componentDidMount() {
         fetch(
             "C:\\Users\\alumne-DAM\\Documents\\Lenguaje-Marcas\\Lenguaje\\Activitat3.15\\data.json")
             .then((res) => res.json())
             .then((json) => {
                 this.setState({
-                    items : json,
+                    items: json,
                     DataisLoaded: true
                 });
             });
     }
-    render(){
+    render() {
         const { DataisLoaded, items } = this.state;
         if (!DataisLoaded) return <div>
             <h1>Por favor espere un poco...</h1></div>;
 
-            return (
-                <div className="App">
-                    <h1> Fetch data from an api in react </h1> {
-                        items.datosOferta.map((item) => (
-                            <ol key={item.id} >
-                                User_Name: {item.nombreOferta} 
-                            </ol>
-                        ))
-                    }
-                </div>
-            );
+        return (
+            <div className="App">
+                <h1> Fetch data from an api in react </h1> {
+                    items.datosOferta.map((item) => (
+                        <ol key={item.id} >
+                            User_Name: {item.nombreOferta}
+                        </ol>
+                    ))
+                }
+            </div>
+        );
     }
 }
-export default App
+}
+export default chocolates
