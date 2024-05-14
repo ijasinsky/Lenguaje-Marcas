b24af4b9d13f64c8332fa098700fa30ad0814f10
ijasinsky <ijasinsky@nigul.cide.es>
2024-05-14 18:53:56 +0200

    act6.1casi

--- express/index.js
+++ express/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const db = require('better-sqlite3')('personas.sqlite');
+const db = require('better-sqlite3')('usuari.sqlite');
+const dbp = require('better-sqlite3')('producte.sqlite');
+const dbc = require('better-sqlite3')('comandes.sqlite');
 
 const app = express()
 const port = 3000
@@ -18,31 +20,54 @@ app.get('/', (req, res) => {
   res.render("index", msgs= { msgs: []});
 })
 
-app.get('/personas', (req, res) => {
+app.get('/usuaris', (req, res) => {
     //Select
-    //const rows = db.prepare('Select * from personas').all();
+    const rows = db.prepare('Select * from usuari').all();
     res.send(rows)
   })
 
-app.get('/persona', (req, res) => {
+app.get('/usuari', (req, res) => {
     //Select
     /*personaID = req.query.id;
-    const row = db.prepare('SELECT * from personas WHERE id = ?').get(personaID);
-    res.json(row)*/
-    res.render("persona");
+    const rowp = db.prepare('SELECT * from personas WHERE id = ?').get(personaID);
+    res.json(rowp)*/
+    usuariid = req.query.id;
+    const row = db.prepare('SELECT * FROM usuari WHERE id = ?').get(usuariid);
+    res.json(row);
+    res.render("usuari");
   })
-  app.post('/persona'), (req, res) => {
-    /*personaID = req.body.id;
-    const row = db.prepare('SELECT * from personas WHERE id = ?').get(personaID);
-    res.json(row)*/
 
-    if(req.body.nombre && req.body.apellidos){
-      const insert = db.prepare("INSERT INTO presonas (nombre, apellidos) values(?, ?)")
-      const info = insert.run(req.body.nombre, req.body.apellidos);
+app.get('/productes', (req, res) => {
+    //Select
+    const rowsp = dbp.prepare('Select * from producte').all();
+    res.send(rowsp)
+  })
+
+app.get('/producte', (req, res) => {
+    producteid = req.query.id;
+    const rowp = dbp.prepare('SELECT * FROM producte WHERE id = ?').get(producteid);
+    res.json(rowp);
+    res.render("producte");
+  })
+
+app.get('/comanda', (req, res) => {
+    //Select
+    const rowsp = dbp.prepare('Select * from comandes').all();
+    res.send(rowsp)
+  })
+
+  //Ignorar de momento
+  /*app.post('/usuari'), (req, res) => {
+    //personaID = req.body.id;
+    //const row = db.prepare('SELECT * from personas WHERE id = ?').get(personaID);
+    //res.json(row)
+    if(req.body.nom && req.body.email){
+      const insert = db.prepare("INSERT INTO usuari (nom, email) values(?, ?)")
+      const info = insert.run(req.body.nom, req.body.email);
         console.log(info);
     }
-    res.redirect("persona");
-  }
+    res.redirect("usuari");
+  }*/
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
